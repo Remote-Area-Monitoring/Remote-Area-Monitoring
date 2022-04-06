9ab2db385b3e2ca22d388e38c2b3e3f24706658c
wv040 <wvining12@gmail.com>
2022-04-06 01:32:33 -0400

    Reverse engineered commands to receive image data -- write image to file

--- source/prototype/python_aggregator/cam_test.py
+++ source/prototype/python_aggregator/cam_test.py
@@ -11,11 +11,27 @@ import json
 def main():
     port = serial.Serial('COM3', 115200, timeout=1)
     time.sleep(2)
-    port.write(b'0\r\n')
-    bmp = port.readall()
-    file = open('test_img.bmp', 'wb')
-    file.write(bmp)
+    # 01: 19:11.697 -> 2
+    # 01: 19:11.697 -> 16
+    # 01: 19:11.697 -> 45
+    port.write(b'\x02')
+    port.write(b'\x10')
+    port.write(b'\x2D')
+    data = port.readall()
+    print(data.find(b'\xff\xd8'))
+    print(data.find(b'\xff\xd9'))
+    print(data)
+    # file = open('test_img.', 'wb')
+    # file.write(data)
+    image = bytearray()
+    for i in range(data.find(b'\xff\xd8'), data.find(b'\xff\xd9') + 1):
+        image.append(data[i])
+    print('writing image file')
+    with open('test_img2.jpg', 'wb') as file:
+        file.write(bytes(image))
     print('done')
+
+
 
 if __name__ == "__main__":
     main()
