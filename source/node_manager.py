4430cadb69d8d7a97c58c96697dd65492e315b5a
wv040 <wvining12@gmail.com>
2022-05-24 19:10:50 -0400

    Adds module for managing nodes - add nodes to configuration database

---
+++ source/node_manager.py
@@ -0,0 +1,32 @@
+from database import Database
+from timekeeper import Timestamps
+from settings import Settings
+
+
+class Nodes:
+    def __init__(self):
+        self.ts = Timestamps()
+        self.config = Settings('general.config')
+        self.db = Database(self.config.get_setting('databases', 'nodes_db_path'))
+
+    def add_node(self, node_id, status, lat, lon, node_config: dict = None):
+        dataobj = {
+            'node_id': node_id,
+            'status': status,
+            'lat': lat,
+            'lon': lon,
+            'node_config': node_config,
+            'date_created': self.ts.get_timestamp(),
+            'date_last_modified': self.ts.get_timestamp()
+        }
+        self.db.insert(dataobj)
+
+
+def main():
+    nodes = Nodes()
+    # Adds dev prototype node
+    nodes.add_node(2222631473, 'active', 28.316450, -80.726982)
+
+
+if __name__ == '__main__':
+    main()
