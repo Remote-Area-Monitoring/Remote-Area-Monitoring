2570bd92f35884287c1ffdfcb2b6e8e1e3c7e458
wv040 <wvining12@gmail.com>
2022-06-04 18:07:36 -0400

    Adds example map data - example nodes multipoint map -- new maps example page for testing maps and styles

--- source/website/map.py
+++ source/website/map.py
@@ -1,9 +1,43 @@
 from dash import Dash, dcc, html, Input, Output, State
 import dash_bootstrap_components as dbc
 import plotly.express as px
+from source.util.database import Database
+from source.util.settings import Settings
+import pandas as pd
+from source.util.timekeeper import Timestamps
 
 
 class Map:
+    def __init__(self):
+        self.ts = Timestamps()
+        self.config = Settings('general.config')
+        self.nodes_db = Database(self.config.get_setting('databases', 'nodes_db_path'))
+        self.sensors_db = Database(self.config.get_setting('databases', 'sensor_data_db_path'))
+
+    def __get_node_location_dataframe(self):
+        data = self.nodes_db.get_all()
+        for record in data:
+            record.pop('node_config')
+            record['date_created'] = self.ts.get_time_date_string(record['date_created'])
+            record['date_last_modified'] = self.ts.get_time_date_string(record['date_last_modified'])
+            record['size'] = 5
+        print(data)
+        return pd.DataFrame(data)
+
+    def get_all_nodes_map_div(self, zoom=12):
+        data = self.__get_node_location_dataframe()
+        fig = px.scatter_mapbox(data, lat="lat", lon="lon", color_discrete_sequence=["blue"], zoom=zoom, size='size',
+                                size_max=15.0)
+        fig.update_layout(mapbox_style="open-street-map")
+        fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+        # fig.show()
+        div = html.Div([
+            dcc.Graph(figure=fig)
+            ],
+            style={'display': 'flex', 'justifyContent': 'center'}
+        )
+        return div
+
     def get_single_point_map_div(self, latitude: float, longitude: float, size: float = 5.0, zoom=15):
         point = [{'lat': latitude, 'lon': longitude, 'point_size': size}]
         fig = px.scatter_mapbox(point, lat="lat", lon="lon", color_discrete_sequence=["blue"], zoom=zoom,
@@ -15,5 +49,23 @@ class Map:
         ])
         return div
 
-    def get_multi_point_map_div(self):
-        pass
+    def test_multi_point_map(self):
+        df = self.__get_node_location_dataframe()
+        pd.set_option("display.max_rows", None, "display.max_columns", None)
+        print(df)
+        fig = px.scatter_mapbox(df,
+                                lat='lat',
+                                lon='lon',
+                                hover_name='node_id',
+                                zoom=1)
+
+        fig.show()
+
+
+def main():
+    map = Map()
+    map.test_multi_point_map()
+
+
+if __name__ == '__main__':
+    main()
