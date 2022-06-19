45bc9fc13816fecbfde73d59e7cd5840adb617d4
wv040 <wvining12@gmail.com>
2022-06-19 15:38:07 -0400

    Adds image data task to updater

--- source/website/pages/updater.py
+++ source/website/pages/updater.py
@@ -6,6 +6,7 @@ class Updater:
     def __init__(self, mesh: Mesh):
         self.mesh = mesh
         self.mesh.update_nodes_sensor_data()
+        self.mesh.update_nodes_image_data()
 
     def get_layout(self):
         layout = html.Div([
