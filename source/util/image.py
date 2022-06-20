a60f39946a6c92c92e7b1fd27550bf092bd6a6e7
wv040 <wvining12@gmail.com>
2022-06-20 15:31:26 -0400

    Clears old data from images db -- updates image div to return list of divs

--- source/util/image.py
+++ source/util/image.py
@@ -27,21 +27,26 @@ class Image:
     def get_all_images(self):
         pass
 
-    def test_image(self):
-        images = self.db.get_all()
-        print(len(images))
-        if images is None:
-            return None
-        image = images[-1]
-        pixels = image['pixels']
-        jpg = bytearray(pixels)
-        buf = io.BytesIO(jpg)
-        img = pil.open(buf)
-        fig = px.imshow(img)
-        fig.show()
+    # def test_image(self):
+    #     images = self.db.get_all()
+    #     print(len(images))
+    #     if images is None:
+    #         return None
+    #     image = images[-1]
+    #     pixels = image['pixels']
+    #     jpg = bytearray(pixels)
+    #     buf = io.BytesIO(jpg)
+    #     img = pil.open(buf)
+    #     fig = px.imshow(img)
+    #     fig.show()
 
-    def get_test_image_div(self):
-        images = self.db.get_all()
+    def get_test_image_div(self, node_id=None):
+        if node_id is None:
+            images = self.db.get_all()
+        else:
+            images = self.db.get_data_single_field('node_id', node_id)
+        if len(images) < 1:
+            return None
         print(len(images))
         if images is None:
             return None
@@ -51,16 +56,24 @@ class Image:
         buf = io.BytesIO(jpg)
         img = pil.open(buf)
         fig = px.imshow(img)
-        div = html.Div([
-            dcc.Graph(figure=fig)
+        title_div = html.Div([
+            html.H2(str(node_id))
             ],
             style={'display': 'flex', 'justifyContent': 'center'}
         )
-        return div
+        divs = [title_div, html.Br(), dcc.Graph(figure=fig)]
+        # div = html.Div([
+        #     html.H2(str(node_id)),
+        #     html.Br(),
+        #     dcc.Graph(figure=fig)
+        #     ],
+        #     style={'display': 'flex', 'justifyContent': 'center'}
+        # )
+        return divs
 
 def main():
     img = Image()
-    img.test_image()
+    # img.test_image()
 
 
 if __name__ == '__main__':
