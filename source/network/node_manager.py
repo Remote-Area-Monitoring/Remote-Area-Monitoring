bf1c43204f553d57691694f21893db4b890bd4fc
wv040 <wvining12@gmail.com>
2022-06-22 15:25:38 -0400

    Adds method to update node status -- updates node generation main method

--- source/network/node_manager.py
+++ source/network/node_manager.py
@@ -12,7 +12,7 @@ class Nodes:
         else:
             self.db = database_obj
 
-    def add_node(self, node_id, status, lat, lon, node_config: dict = None):
+    def add_node(self, node_id, status, lat, lon, node_config: dict = None, notes=None):
         existing_record = self.db.get_data_single_field('node_id', node_id)
         if len(existing_record) > 0:
             return False
@@ -22,11 +22,15 @@ class Nodes:
             'lat': lat,
             'lon': lon,
             'node_config': node_config,
+            'notes': notes,
             'date_created': self.ts.get_timestamp(),
             'date_last_modified': self.ts.get_timestamp()
         }
         self.db.insert(dataobj)
         return True
+
+    def add_node_dataobj(self, node_config_dataobj):
+        self.db.insert(node_config_dataobj)
 
     def remove_node(self, node_id):
         dataobj = {
@@ -37,11 +41,29 @@ class Nodes:
             return True
         return False
 
+    def update_node_status(self, node_id, status):
+        current_config = self.db.get_data_single_field('node_id', node_id)
+        if len(current_config) > 0:
+            current_config = current_config[0]
+            current_config['status'] = status
+            current_config['date_last_modified'] = self.ts.get_timestamp()
+            self.remove_node(node_id)
+            self.add_node_dataobj(current_config)
+            return True
+        return False
+
 
 def main():
     nodes = Nodes()
     # Adds dev prototype node
-    print(nodes.add_node(2222631473, 'active', 28.316450, -80.726982))
+    print(nodes.add_node(2222631473, 'active', 28.606798, -81.200847, node_config={'sensors': True, 'camera': False},
+                         notes='Hand wired prototype'))
+    print(nodes.add_node(4144723677, 'active', 28.602916, -81.192481, node_config={'sensors': True, 'camera': False},
+                         notes='First engineering sample'))
+    print(nodes.add_node(4146216805, 'active', 28.592791, -81.200135, node_config={'sensors': True, 'camera': True},
+                         notes='Second engineering sample'))
+    print(nodes.add_node(4144717489, 'active', 28.602000, -81.206240, node_config={'sensors': True, 'camera': True},
+                         notes='Third engineering sample'))
     # print(nodes.remove_node(2222631473))
     # Adds proto 1 node
     # nodes.add_node(4144723677, 'active', 28.316450, -80.726982)
