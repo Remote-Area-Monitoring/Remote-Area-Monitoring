53185559d70a545d7888f28f2089630ac5be2b9d
wv040 <wvining12@gmail.com>
2022-06-25 19:30:07 -0400

    Adds image view -- fixes connection status to match network -- adds node health readouts

--- source/website/pages/node_details.py
+++ source/website/pages/node_details.py
@@ -1,22 +1,26 @@
 from source.util.analysis import Analysis
 from source.network.mesh import Mesh
 from source.util.database import Database
-from dash import html
+from dash import html, dcc
 import dash_bootstrap_components as dbc
 import dash_daq as daq
 from source.util.settings import Settings
 from source.util.conversions import Convert
 from source.website.map import Map
+from source.util.image import Image
+from source.util.timekeeper import Timestamps
 
 
 class NodeDetails:
-    def __init__(self, pathname: str, mesh: Mesh, nodes_db: Database, sensors_db: Database):
-        self.mesh = mesh
-        self.nodes_db = nodes_db
-        self.map = Map()
+    def __init__(self, pathname: str):
         self.config = Settings('general.config')
+        self.connected_nodes_config = Settings('nodes.config')
+        self.ts = Timestamps()
+        self.nodes_db = Database(self.config.get_setting('databases', 'nodes_db_path'))
+        self.sensors_db = Database(self.config.get_setting('databases', 'sensor_data_db_path'))
+        self.map = Map()
+        self.image = Image()
         self.convert = Convert()
-        self.sensors_db = sensors_db
         self.node_id = self.__get_node_id(pathname)
         self.node_config = self.__get_node_config()
         self.sensor_data = self.__get_node_sensor_data()
@@ -241,28 +245,146 @@ class NodeDetails:
             dbc.Col([
                 self.map.get_single_point_map_div(latitude=self.node_config['lat'], longitude=self.node_config['lon'],
                                                   zoom=15, style='stamen-terrain')
-            ], width='auto')
+            ], width='auto'),
         ], justify='center')
         rows.append(map_row)
+
+        spacer_row = dbc.Row([dbc.Col([html.Hr()], width='auto')], justify='center')
+
+        if self.node_config['node_config']['camera']:
+            rows.append(spacer_row)
+            picture_heading_row = dbc.Row([dbc.Col([html.H3('Node Images')], width='auto')], justify='center')
+            rows.append(picture_heading_row)
+
+            options = self.image.get_image_list(self.node_id)
+            if options is None or len(options) < 1:
+                options = ['No Images Available']
+            image_selection_row = dbc.Row([
+                dbc.Col([
+                    dcc.Dropdown(options, value=options[0], searchable=True, clearable=False, id='node-detail-image-drop')
+                ], width='4')
+            ], justify='center')
+            rows.append(image_selection_row)
+
+            image_row = dbc.Row([
+                dbc.Col([
+                    html.Div(id='node-detail-image-view')
+                ], width='auto')
+            ], justify='center')
+            rows.append(image_row)
 
         row_5 = dbc.Row([dbc.Col([html.Hr()], width='auto')], justify='center')
         rows.append(row_5)
 
         row_6 = dbc.Row([dbc.Col([html.H2('Node Health')], width='auto')], justify='center')
         rows.append(row_6)
-        rows.append(row_2)
+        # rows.append(row_2)
 
         # TODO: give the user a way to edit node config
-        # TODO: Actually detect connection status
-        connection_status = 'connected'
-        connection_color = 'green'
+        connected_nodes = self.connected_nodes_config.get_list_setting('connected_nodes', 'node_ids')
+        connection_last_updated = self.connected_nodes_config.get_float_setting('connected_nodes', 'last_updated')
+        sensor_data_last_update = next(record for record in self.analyzed_data if record['name'] == 'timestamp')
+        # print(sensor_data_last_update)
+        if sensor_data_last_update['max_value'] > connection_last_updated:
+            connection_last_updated = sensor_data_last_update['max_value']
+        last_updated_string = self.ts.get_long_timestring(connection_last_updated)
+        if self.node_id in connected_nodes:
+            connection_status = 'Connected'
+            connection_color = 'green'
+        else:
+            connection_status = 'Disconnected'
+            connection_color = 'red'
+
+        last_updated_row = dbc.Row([dbc.Col([html.P('Date Last Updated: ' + last_updated_string)],
+                                 width='auto')],
+                        justify='center')
+        rows.append(last_updated_row)
+
         connection_status_row = dbc.Row([
             dbc.Col([
-                html.H3('Status: ' + connection_status, style={'color': connection_color})
+                html.H3('Network Status: ' + connection_status, style={'color': connection_color}),
+                html.H3('Polling Status: ' + self.node_config['status'])
             ], width='auto')
         ], justify='center')
         rows.append(connection_status_row)
 
+        break_row = dbc.Row([dbc.Col([html.Br()], width='auto')], justify='center')
+        rows.append(break_row)
+
+        voltage_data = next(record for record in self.analyzed_data if record['name'] == 'bus_voltage_V')
+        voltage = voltage_data['current_value']
+        volts_min = self.config.get_float_setting('units', 'volts_min')
+        volts_max = self.config.get_float_setting('units', 'volts_max')
+        if 2.8 < voltage < 4.3:
+            volts_color = '#02b502'
+        else:
+            volts_color = '#ff4242'
+        if voltage > volts_max:
+            volts_max = voltage
+        if voltage < volts_min:
+            volts_min = voltage
+
+        current_data = next(record for record in self.analyzed_data if record['name'] == 'current_mA')
+        current = current_data['current_value']
+        current_min = self.config.get_float_setting('units', 'current_min')
+        current_max = self.config.get_float_setting('units', 'current_max')
+        if current > 0:
+            current_label = 'Battery Current -> Charging'
+            current_color = '#02b502'
+        else:
+            current_label = 'Battery Current -> Discharging'
+            current_color = '#ff4242'
+        if current > current_max:
+            current_max = current
+        if current < current_min:
+            current_min = current
+
+        power_data = next(record for record in self.analyzed_data if record['name'] == 'power_mW')
+        power = round(power_data['current_value'] / 1000, 3)
+        power_color = {"gradient": True, "ranges": {"green": [0, 2], "yellow": [2, 2.8], "red": [2.8, 3]}}
+
+        power_status_row = dbc.Row([
+            dbc.Col([
+                daq.Tank(
+                    value=voltage,
+                    units='Volts',
+                    color=volts_color,
+                    showCurrentValue=True,
+                    max=volts_max,
+                    min=volts_min,
+                    label='Battery Voltage'
+                )
+            ], width='auto'),
+            dbc.Col([
+                daq.Gauge(
+                    value=current,
+                    units='milliamps',
+                    color=current_color,
+                    showCurrentValue=True,
+                    max=current_max,
+                    min=current_min,
+                    label=current_label
+                )
+            ], width='auto'),
+            dbc.Col([
+                daq.Gauge(
+                    value=power,
+                    units='watts',
+                    color=power_color,
+                    showCurrentValue=True,
+                    max=3,
+                    min=0,
+                    label='Power'
+                )
+            ], width='auto')
+        ], justify='center')
+        rows.append(power_status_row)
+
+        # TODO: Add tank for battery
+        # TODO: Add gauge for current -600 to +600 - change color based on charge / discharge
+        # TODO: Add gauge for power
+
+        rows.append(spacer_row)
         return rows
 
     def get_layout(self):
