890d9911f64ececefc992e5223ee355f22c0dfd0
wv040 <wvining12@gmail.com>
2022-06-29 00:40:02 -0400

    Adds proto network map - limited to a vertical only map

--- source/website/map.py
+++ source/website/map.py
@@ -5,12 +5,15 @@ from source.util.database import Database
 from source.util.settings import Settings
 import pandas as pd
 from source.util.timekeeper import Timestamps
+import json
+import plotly.graph_objects as go
 
 
 class Map:
     def __init__(self):
         self.ts = Timestamps()
         self.config = Settings('general.config')
+        self.nodes_config = Settings('nodes.config')
         self.nodes_db = Database(self.config.get_setting('databases', 'nodes_db_path'))
         self.sensors_db = Database(self.config.get_setting('databases', 'sensor_data_db_path'))
 
@@ -67,6 +70,64 @@ class Map:
                                 hover_name='node_id',
                                 zoom=1)
 
+        fig.show()
+
+    def get_connection_tree(self, subs, lats, lons):
+        # print(subs)
+        for sub in subs:
+            node_id = sub['nodeId']
+            node_data = self.nodes_db.get_data_single_field('node_id', node_id)
+            if len(node_data) > 0:
+                node_data = node_data[0]
+                lats.append(node_data['lat'])
+                lons.append(node_data['lon'])
+            if 'subs' in sub:
+                self.get_connection_tree(sub['subs'], lats, lons)
+        location_data = {
+            'lats': lats,
+            'lons': lons
+        }
+        return location_data
+
+    def network_map(self):
+        lats = list()
+        lons = list()
+        topology = self.nodes_config.get_setting('connected_nodes', 'topology')
+        if topology is None:
+            return html.Div([])
+        topology = json.loads(topology)
+        # Example topology
+        # topology = {'nodeId': 2222635529, 'root': True, 'subs': [{'nodeId': 4144717489,
+        #                                                           'subs': [{'nodeId': 4146216805},
+        #                                                                    {'nodeId': 4144723677,
+        #                                                                     'subs': [{'nodeId': 2222631473}]}]}]}
+        root = topology['nodeId']
+        if self.config.get_int_setting('mesh_network', 'root_id') != root:
+            return html.Div([html.P('No data available for root node')])
+        root_lat = self.config.get_float_setting('mesh_network', 'root_lat')
+        lats.append(root_lat)
+        root_lon = self.config.get_float_setting('mesh_network', 'root_lon')
+        lons.append(self.config.get_float_setting('mesh_network', 'root_lon'))
+        if 'subs' in topology:
+            subs = topology['subs']
+        else:
+            subs = []
+        location_data = self.get_connection_tree(subs, lats, lons)
+
+        fig = go.Figure(go.Scattermapbox(
+            mode='markers+lines',
+            lon=location_data['lons'],
+            lat=location_data['lats'],
+            line={'width': 5},
+            marker={'size': 15}
+        ))
+        fig.update_layout(
+            mapbox={
+                'style': 'stamen-terrain',
+                'center': {'lon': root_lon, 'lat': root_lat},
+                'zoom': 15
+            }
+        )
         fig.show()
 
 
