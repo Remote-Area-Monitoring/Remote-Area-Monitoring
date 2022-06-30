168882e9cb4156c2fd4b64609ce0c2afc405e918
wv040 <wvining12@gmail.com>
2022-06-30 01:04:45 -0400

    Adds empty module for mesh network status / management - demo todo list

---
+++ source/website/pages/mesh_manager.py
@@ -0,0 +1,29 @@
+# page features
+    # network status
+        # operational
+        # degraded
+        # offline
+    # quick controls
+        # disable polling for sensors, images, connection status, all
+        # start / restart updater?
+    # settings modal
+        # expose all general.config -> mesh_network
+    # map of nodes
+        # hover data
+            # title: status
+            # node id: node_id
+            # last updated
+            # signal strength
+            # alert status -> not implimented
+        # color of node point
+    # printout mesh topology
+    # table displaying nodes
+    # alerts table -> not implimented
+
+# to do before demo
+    # update nodes with most signal strength addition
+    # impliment sunrise / sunset setting for camera
+    # impliment mesh network page
+    # add a map to node overview
+
+
