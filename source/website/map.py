eda7d0ac4fe83afdff08c652a6a8c0d6f4048888
wv040 <wvining12@gmail.com>
2022-06-30 23:16:17 -0400

    Adds methods to get node network status graph -- adds proto heatmap

--- source/website/map.py
+++ source/website/map.py
@@ -7,6 +7,7 @@ import pandas as pd
 from source.util.timekeeper import Timestamps
 import json
 import plotly.graph_objects as go
+from statistics import mean
 
 
 class Map:
@@ -130,10 +131,137 @@ class Map:
         )
         fig.show()
 
+    def __get_node_status_map_dataframe(self):
+        data = list()
+        nodes = self.nodes_db.get_all()
+        connected_nodes = self.nodes_config.get_list_setting('connected_nodes', 'node_ids')
+        if len(connected_nodes) == 0:
+            root_connection_status = 'Disconnected'
+            root_signal_strength = 'None'
+            root_polling_status = 'Inactive'
+        else:
+            root_connection_status = 'Connected'
+            root_signal_strength = self.nodes_config.get_int_setting('connected_nodes', 'root_signal_strength')
+            polling = self.config.get_bool_setting('mesh_network', 'polling')
+            if polling is True:
+                root_polling_status = 'Active'
+            else:
+                root_polling_status = 'Inactive'
+        root_id = self.config.get_int_setting('mesh_network', 'root_id')
+        root_lat = self.config.get_float_setting('mesh_network', 'root_lat')
+        root_lon = self.config.get_float_setting('mesh_network', 'root_lon')
+        dataobj = {
+            'node_id': root_id,
+            'lat': root_lat,
+            'lon': root_lon,
+            'connection_status': root_connection_status,
+            'signal_strength(dB)': root_signal_strength,
+            'polling_status': root_polling_status,
+            'notes': 'Root Node',
+            'size': 5
+        }
+        data.append(dataobj)
+
+        for node in nodes:
+            sensor_data = self.sensors_db.get_data_single_field('node_id', node['node_id'],
+                                                                self.ts.get_1week_timestamp())
+            if len(sensor_data) < 1:
+                sensor_data = self.sensors_db.get_data_single_field('node_id', node['node_id'])
+            try:
+                sensor_data = sensor_data[-1]
+            except IndexError:
+                return None
+            connection_status = 'Disconnected'
+            signal_strength = 'None'
+            if node['node_id'] in connected_nodes:
+                connection_status = 'Connected'
+                signal_strength = sensor_data['connection_strength']
+            dataobj = {
+                'node_id': node['node_id'],
+                'lat': node['lat'],
+                'lon': node['lon'],
+                'connection_status': connection_status,
+                'signal_strength(dB)': signal_strength,
+                'polling_status': node['status'],
+                'notes': node['notes'],
+                'size': 5
+            }
+            data.append(dataobj)
+        #     node_ids.append(node['node_id'])
+        #     lats.append(node['lat'])
+        #     lons.append(node['lon'])
+        #     con_status.append(connection_status)
+        #     sig_strength.append(signal_strength)
+        #     poll_status.append(node['status'])
+        #     notes.append(node['notes'])
+        # dataobj = {
+        #     'node_id': node_ids,
+        #     'lat': lats,
+        #     'lon': lons,
+        #     'connection_status': con_status,
+        #     'signal_strength(dB)': sig_strength,
+        #     'polling_status': poll_status,
+        #     'notes': notes
+        # }
+        return pd.DataFrame(data)
+
+    def get_node_status_map(self):
+        data = self.__get_node_status_map_dataframe()
+        if data is None:
+            return html.P('Network Status Map Unavailable')
+        fig = px.scatter_mapbox(data, lat="lat", lon="lon", color='connection_status',
+                                color_discrete_sequence=['green', 'red'], zoom=13, hover_name='node_id',
+                                size='size', hover_data=['lat', 'lon', 'connection_status', 'signal_strength(dB)',
+                                                         'polling_status', 'notes'])
+        fig.update_layout(mapbox_style="stamen-terrain")
+        fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+        fig.update_layout(legend_x=0.01, legend_y=0.99)
+        div = html.Div([
+            dcc.Graph(figure=fig)
+        ])
+        return div
+
+    def get_heatmap(self):
+        lats = list()
+        lons = list()
+        mags = list()
+        nodes = self.nodes_db.get_all()
+        for node in nodes:
+            lats. append(node['lat'])
+            lons.append(node['lon'])
+            data = self.sensors_db.get_data_single_field('node_id', node['node_id'], self.ts.get_24h_timestamp())
+            temperature_data = list()
+            for record in data:
+                if 'calibration_temperature' in record:
+                    temperature_data.append(record['calibration_temperature'])
+                elif 'air_temperature_C' in record:
+                    temperature_data.append(record['air_temperature_C'])
+            mags.append(mean(temperature_data))
+        root_lat = self.config.get_float_setting('mesh_network', 'root_lat')
+        root_lon = self.config.get_float_setting('mesh_network', 'root_lon')
+        lats.append(root_lat)
+        lons.append(root_lon)
+        mags.append(0)
+        dataobj = {
+            'lats': lats,
+            'lons': lons,
+            'mags': mags
+        }
+        print(dataobj)
+
+        df = pd.DataFrame(dataobj)
+        print(df)
+        fig = px.density_mapbox(df, lat='lats', lon='lons', z='mags', radius=100,
+                                center=dict(lat=root_lat, lon=root_lon), zoom=12,
+                                mapbox_style="stamen-terrain")
+        fig.show()
+
+
+
 
 def main():
     map = Map()
-    map.test_multi_point_map()
+    map.get_heatmap()
 
 
 if __name__ == '__main__':
